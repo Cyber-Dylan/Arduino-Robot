3ecc9a66406504a0e69b2a084003cbe6978ee90e
D7x8 <74881467+D7x8@users.noreply.github.com>
2022-07-08 18:29:29 +1200

    Add files via upload

---
+++ Robot.ino
@@ -0,0 +1,51 @@
+#define echoPin 2 // attach pin D2 to pin Echo of HC-SR04
+#define trigPin 3 //attach pin D3 to pin Trig of HC-SR04
+
+// defines variables
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
+const int RIGHT_MOTOR_1 = 10;
+const int RIGHT_MOTOR_2 = 9;
+
+const int LEFT_MOTOR_1 = 8;
+const int LEFT_MOTOR_2 = 7;
+
+const int SENSOR = A0;
+
+void setup() {
+  pinMode(RIGHT_MOTOR_1, OUTPUT);
+  pinMode(RIGHT_MOTOR_2, OUTPUT);
+  pinMode(LEFT_MOTOR_1, OUTPUT);
+  pinMode(LEFT_MOTOR_2, OUTPUT);
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  Serial.println("Ultrasonic Sensor HC-SR04 Test");
+  Serial.println("with Arduino UNO R3");
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.print(distance);
+  Serial.println(" cm");
+  if (distance < 20) {
+    digitalWrite(RIGHT_MOTOR_1, LOW);
+    digitalWrite(RIGHT_MOTOR_2, LOW);
+    digitalWrite(LEFT_MOTOR_1, HIGH);
+    digitalWrite(LEFT_MOTOR_2, LOW);
+
+  }
+  else {
+    digitalWrite(RIGHT_MOTOR_1, HIGH);
+    digitalWrite(RIGHT_MOTOR_2, LOW);
+    digitalWrite(LEFT_MOTOR_1, HIGH);
+    digitalWrite(LEFT_MOTOR_2, LOW);
+  }
+}
